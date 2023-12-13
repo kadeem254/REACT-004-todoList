ab3bd370bf36acc482a320a05195855284f60fe7
kadeem254 <kadeemalexander254@gmail.com>
2023-12-13 11:02:51 +0300

    STYLE: added custom colors for project to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cream: '#E8F4BD',
+        mindaro: "#d9ed92",
+        light_green: "#b5e48c",
+        light_green_2:  "#99d98c",
+        emerald: "#76c893",
+        keppel: "#52b69a",
+        verdigris: "#34a0a4",
+        bondi_blue: "#168aad",
+        cerulean: "#1a759f",
+        lapis_lazuli: "#1e6091",
+        indigo_dye: "#184e77",
+      },
+    },
   },
   plugins: [],
-}
-
+};
